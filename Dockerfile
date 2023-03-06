9e36201b96517e50e800b64cd4014d338045fab9
olegvantsevich <OVantsevich@gmail.com>
2023-03-06 18:32:56 +0300

    06.03.2023 version 1.1

--- Dockerfile
+++ Dockerfile
@@ -23,6 +23,6 @@ WORKDIR /app
 
 COPY --from=builder /app/main /app/main
 
-EXPOSE 12345
+EXPOSE 10000
 
 CMD ["/app/main"]
--- internal/config/mainConfig.go
+++ internal/config/mainConfig.go
@@ -10,19 +10,19 @@ import (
 // MainConfig with init data
 type MainConfig struct {
 	JwtKey string `env:"JWT_KEY,notEmpty" envDefault:"874967EC3EA3490F8F2EF6478B72A756"`
-	Port   string `env:"PORT,notEmpty" envDefault:"99999"`
+	Port   string `env:"PORT,notEmpty" envDefault:"6000"`
 
-	PriceServicePort string `env:"PRICE_SERVICE_PORT,notEmpty" envDefault:"10000"`
+	PriceServicePort string `env:"PRICE_SERVICE_PORT,notEmpty" envDefault:"4000"`
 	PriceServiceHost string `env:"PRICE_SERVICE_HOST,notEmpty" envDefault:"localhost"`
 
-	PaymentServicePort string `env:"PAYMENT_SERVICE_PORT,notEmpty" envDefault:"30000"`
+	PaymentServicePort string `env:"PAYMENT_SERVICE_PORT,notEmpty" envDefault:"2000"`
 	PaymentServiceHost string `env:"PAYMENT_SERVICE_HOST,notEmpty" envDefault:"localhost"`
 
-	UserServicePort string `env:"PAYMENT_SERVICE_PORT,notEmpty" envDefault:"20000"`
-	UserServiceHost string `env:"PAYMENT_SERVICE_HOST,notEmpty" envDefault:"localhost"`
+	UserServicePort string `env:"USER_SERVICE_PORT,notEmpty" envDefault:"1000"`
+	UserServiceHost string `env:"USER_SERVICE_HOST,notEmpty" envDefault:"localhost"`
 
-	TradingServicePort string `env:"PAYMENT_SERVICE_PORT,notEmpty" envDefault:"40000"`
-	TradingServiceHost string `env:"PAYMENT_SERVICE_HOST,notEmpty" envDefault:"localhost"`
+	TradingServicePort string `env:"TRADING_SERVICE_PORT,notEmpty" envDefault:"5000"`
+	TradingServiceHost string `env:"TRADING_SERVICE_HOST,notEmpty" envDefault:"localhost"`
 }
 
 // NewMainConfig parsing config from environment
